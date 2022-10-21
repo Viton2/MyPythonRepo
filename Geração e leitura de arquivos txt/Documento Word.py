d39895d75aacf809083f6a2ee98f033833c1be5f
vitinhocastro2@gmail.com <vitinhocastro2@gmail.com>
2022-10-21 02:10:06 -0300

    Testing

--- Geração e leitura de arquivos txt/Documento Word.py
+++ Geração e leitura de arquivos txt/Documento Word.py
@@ -11,16 +11,15 @@ carga = input("Qual a carga horária? ")
 doc.add_paragraph('Com carga horária de {} horas.'.format(carga), style='List Bullet')
 
 tab = doc.add_table(rows=1, cols=2)
-tab.style="Colorful List Accent 6"
+tab.style = "Colorful List Accent 6"
 cels = tab.rows[0].cells
 cels[0].text = 'Número'
 cels[1].text = "Assunto"
 
 for numero in range(1, 5):
-   dados = tab.add_row().cells
-   assuntos = input("Qual o assunto {}? ".format(numero))
-   dados[0].text = str(numero)
-   dados[1].text = assuntos
+    dados = tab.add_row().cells
+    assuntos = input("Qual o assunto {}? ".format(numero))
+    dados[0].text = str(numero)
+    dados[1].text = assuntos
 
 doc.save('c:\\temp\\meudoc.docx')
-
--- Geração e leitura de arquivos txt/Geração.py
+++ Geração e leitura de arquivos txt/Geração.py
@@ -5,6 +5,6 @@ file = open("tabuada.txt", "w")
 file.write(f"Tabuada do {num}: \n")
 
 for c in range(0, 11):
-   file.write(f"\n {num} X {c} = {num * c}")
+    file.write(f"\n {num} X {c} = {num * c}")
 
 file.close()
